dcedbc4d6288e5c6e141b1c9ce444d36a532ac9a
Fabio Ribeiro <fabiohkd@gmail.com>
2020-05-04 12:41:41 -0300

    Adicionado o arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nome = "Fábio Ribeiro";
+var idade = 42;
+var frase = "O Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão", "Brasil"));
